3c093610893722d416577aa411fd2c888c182a01
Andrew Burns <arwin915@gmail.com>
2017-02-02 09:48:59 -0500

    Initial commit

---
+++ lab04.c
@@ -0,0 +1,29 @@
+/*
+Andrew Burns
+Alexander Vansteel
+CIS 452 10
+2/9/2017
+Lab 04: Blocking Multi-threaded Server
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <signal.h>
+#include <time.h>
+#include <sys/wait.h>
+
+void worker(char*);
+
+
+int main(){
+	char* input;
+	int totalRequests = 0;
+	int running = 1;
+	
+	while(running){
+		
+	}
+	return 0;
+}
